20871511fe69d0c57c18d5b5d377a767468fa001
Dave <uchennadavid2404@gmail.com>
2022-05-11 18:54:39 +0100

    Mongoose issue initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+//jshint esversion:8
+
+const mongoose = require("mongoose");
+
+//Connecting the url
+
+
+mongoose.connect("mongodb://localhost:27017/fruitsDB",{ useNewUrlParser: true,useUnifiedTopology:true});
+const db = mongoose.connection;
+db.on("error",
+console.error.bind(console,"MongoDB connection error:"));
+
+
+// main().catch(err => console.log(err));
+//
+// async function main(){
+//   await mongoose.connect("mongodb://localhost:27017/fruitsDB");
+// }
+
+const fruitSchema = new mongoose.Schema({
+  name :String,
+  rating:Number,
+  review : String
+});
+
+const Fruit = mongoose.model("Fruit",fruitSchema);
+
+const fruit = new Fruit({
+    name : "Apple",
+    rating: 7,
+    review : "Decent fruit"
+});
+
+fruit.save().then(()=> console.log("Created"));
